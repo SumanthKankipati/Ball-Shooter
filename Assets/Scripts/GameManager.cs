7607d8a476a83bcfa6b3cd91fafe9ca056430091
Sumanth <50363635+SumanthKankipati@users.noreply.github.com>
2019-12-05 21:12:56 +0530

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject winText;
+    public GameObject restartButton;
+    int score = 0;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ScoreUp()
+    {
+        score++;
+        if(score>=4)
+        {
+            win();
+        }
+    }
+    void win()
+    {
+        winText.SetActive(true);
+        restartButton.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
